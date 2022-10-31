7d6eabdb08dc56c827ab946482307ad381e3c989
Vincent Langat <kimutailangat033@gmail.com>
2022-10-31 12:39:08 +0300

    modify admin to add log out button

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,15 +1,96 @@
-import React from 'react'
-
-import Allmeals from './Allmeals';
+// import React,{use} from 'react'
+// import Allmeals from './Allmeals';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, NavLink, useParams, useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Admin() {
+  const params = useParams();
+  const [meal, setMeals] = useState([]);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetch("/meals")
+      .then((response) => response.json())
+      .then((data) => setMeals(data));
+  }, []);
+
+  React.useEffect(() => {
+    console.log("params", params);
+  }, [params]);
+
+  function handleDestroySession(e) {
+    e.preventDefault();
+    fetch("/logout", {
+      method: "DELETE"
+    })
+    .then((r) => {
+      if (r.ok) {
+        navigate("/")
+      
+ 
+
+      }
+      else{
+        alert()
+
+      }
+    });
+    
+
+  }
+
   return (
     <div>
+      <div className="home-1">
+        <Navbar />
+        <div className="navs">
+        <NavLink to={"/"} exact="true" className="btn-view-2">
+          View orders
+        </NavLink>
 
-    <Allmeals />
-  
+        <NavLink to={"/newmealform"} exact="true" className="btn-view-2">
+        Add Meals
+      </NavLink>
+      <button className="btn-view-2" onClick={handleDestroySession}>
+        Log Out
+      </button>
+
+      </div>
+      </div>
+      <div className="meals-board">
+        <h1 className="board">Meals Board</h1>
+      </div>
+
+      <div className="meals-container">
+        {meal.map((meals) => (
+          <div className="meals-dets" key={meals.id}>
+            <div className="meals-image">
+              <img src={meals.image_url} alt="article url" />
+            </div>
+            <div className="meals-det">
+              <h2>
+                <span>{meals.name}</span>
+              </h2>
+              <h3>Ksh{meals.price}</h3>
+              <div className="btn-meals">
+                <Link to={`/allmealsdetails/${meals.id}`}>
+                  <button className="btn-view">View</button>
+                </Link>
+
+              
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+
+  
+  
+  
 }
 
 export default Admin
